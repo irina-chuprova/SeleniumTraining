1ac241a1932b4cc8cbe4b6c7bcdc1d50f586410d
Irina Chuprova <chupirinka@gmail.com>
2016-12-14 14:58:13 +0300

    Задание1: SimpleTest

---
+++ SimpleTest/SimpleTest/SimpleTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.IE;
+
+namespace SimpleTest
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "http://software-testing.ru/";
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
