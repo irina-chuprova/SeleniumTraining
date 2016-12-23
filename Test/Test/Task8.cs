db669f50093cb72c4b5db72ec540f9cd38545e97
Irina Chuprova <chupirinka@gmail.com>
2016-12-23 23:54:44 +0300

    Задание8: Sticker exists for each elements

---
+++ Test/Test/Task8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test8
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void TestStickers()
+        {
+            driver.Url = "https://localhost/litecart";
+            var elements = driver.FindElements(By.CssSelector("div.content li.product"));         
+            foreach(var element in elements)
+            {
+                var countOfSticker = element.FindElements(By.CssSelector("div.sticker")).Count;
+                Assert.AreEqual(1, countOfSticker);
+            }
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
